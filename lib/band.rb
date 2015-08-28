2da66d7f54a723cea13ba46300c18a2598e2f9d4
AlexAlt <alexandra.m.altieri@gmail.com>
2015-08-28 11:35:47 -0700

    Edit bands table to include city and state as separate attributes. Edits specs accordingly

--- lib/band.rb
+++ lib/band.rb
@@ -2,7 +2,7 @@ class Band < ActiveRecord::Base
   has_and_belongs_to_many(:venues)
   validates(:name, :presence => true)
 
-  before_save(:capitalize_name, :capitalize_city)
+  before_save(:capitalize_name, :capitalize_city, :capitalize_state)
 
   private
 
@@ -17,5 +17,14 @@ class Band < ActiveRecord::Base
 
   define_method(:capitalize_city) do
     self.city = (city().capitalize!())
+  end
+
+  define_method(:capitalize_state) do
+    state = self.state
+    capitalize_state = state.split("")
+    capitalize_state.each() do |state|
+        state.capitalize!()
+      end
+    self.state = capitalize_state.join("")
   end
 end
--- spec/band_spec.rb
+++ spec/band_spec.rb
@@ -4,17 +4,22 @@ describe(Band) do
   it { should have_and_belong_to_many (:venues) }
 
   it('returns false if there is no name') do
-    test_band = Band.new({:name => "", :hometown => "Portland, OR"})
+    test_band = Band.new({:name => "", :city => "Portland", :state => "OR"})
     expect(test_band.save()).to(eq(false))
   end
 
    it('capitalizes the band name')  do
-    test_band = Band.create({:name => "blind lovejoy", :hometown => "portland"})
+    test_band = Band.create({:name => "blind lovejoy", :city => "Portland", :state => "OR"})
     expect(test_band.name()).to(eq("Blind Lovejoy"))
   end
 
-  it('capitalizes the band hometown')  do
-    test_band = Band.create({:name => "blind lovejoy", :hometown => "portland"})
-    expect(test_band.hometown()).to(eq("Portland"))
+  it('capitalizes the band city')  do
+    test_band = Band.create({:name => "blind lovejoy", :city => "portland", :state => "OR"})
+    expect(test_band.city()).to(eq("Portland"))
+  end
+
+  it('capitalizes the band state')  do
+    test_band = Band.create({:name => "blind lovejoy", :city => "portland", :state => "or"})
+    expect(test_band.state()).to(eq("OR"))
   end
 end
